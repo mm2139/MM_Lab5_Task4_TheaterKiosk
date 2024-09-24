3449f8d096638f08c46beb487bde7f1d38dd7b32
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-24 08:39:11 -0400

    fin com

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,28 @@
-public class Main {
+import java.util.Scanner;
+
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        int userAge;
+
+        System.out.println("Hello - what is your age?");
+
+        if (scan.hasNextInt()) {
+            userAge = scan.nextInt();
+            if (userAge >= 21) {
+                 System.out.println("You are 21 or over. You get a wristband.");
+            } else if (userAge < 0){
+                System.out.println("You have inputted an invalid response.");
+                System.exit(0);
+            } else {
+                System.out.println("You are under 21. You do not get a wristband.");
+            }
+        } else {
+            System.out.println("You have inputted an invalid response.");
+            System.exit(0);
+        }
+
     }
 }
